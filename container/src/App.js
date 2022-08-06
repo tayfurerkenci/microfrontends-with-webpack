ab7392564f8ed96c0812346f5b8725fbde9aca56
Tayfur Erkenci <terkenci@gmail.com>
2022-08-06 13:02:17 +0300

    production classnames randomized

--- container/src/App.js
+++ container/src/App.js
@@ -2,15 +2,27 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+// Whenever we start to build our application for production
+// rather than generating all these different class names with a prefix 'jss',
+// it's going to instead generate them with a prefix 'ma'
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <hr />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
+
+// Whenever we start to build our application for production
+// rather than generating all these different class names with a prefix 'jss',
+// it's going to instead generate them with a prefix 'ma'
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
 
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
