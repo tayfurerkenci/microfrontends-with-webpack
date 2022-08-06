c6c6b51fb3b44120cbb556ab8b0be0681aecfcb5
Tayfur Erkenci <terkenci@gmail.com>
2022-08-06 15:41:03 +0300

    browser history and two way routing implemented

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -7,7 +7,7 @@ export default () => {
   const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current, {
+    const { onParentNavigate } = mount(ref.current, {
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
 
@@ -16,6 +16,9 @@ export default () => {
         }
       },
     });
+
+    // browser history listens sub apps
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
